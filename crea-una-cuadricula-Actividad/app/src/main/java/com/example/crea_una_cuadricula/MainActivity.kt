6f8d09794e04c52937a5413aae7f73b4d422fe0a
RaphaChi08 <s20120207@alumnos.itsur.edu.mx>
2023-09-21 19:57:09 -0600

    Practica: Crea una cuadrícula. Paso 4: Creando cuadriculas

--- crea-una-cuadricula-Actividad/app/src/main/java/com/example/crea_una_cuadricula/MainActivity.kt
+++ crea-una-cuadricula-Actividad/app/src/main/java/com/example/crea_una_cuadricula/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.example.crea_una_cuadricula
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.crea_una_cuadricula.data.DataSource
 import com.example.crea_una_cuadricula.model.Cuadricula
 import com.example.crea_una_cuadricula.ui.theme.Crea_una_cuadriculaTheme
 
@@ -36,16 +55,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun cuadriculaApp(modifier: Modifier = Modifier){
-
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        modifier = modifier
+    ) {
+        items(DataSource.cuadriculas) { cuadricula ->
+            CuadriculaCard(cuadricula)
+        }
+    }
 }
 
 @Composable
 fun CuadriculaCard(cuadricula: Cuadricula, modifier: Modifier = Modifier){
-
+    Card {
+        Box{
+            Image(
+                painter = painterResource(id = cuadricula.imageRes),
+                contentDescription = null,
+                modifier = modifier
+                    .size(width = 68.dp, height = 68.dp)
+                    .aspectRatio(1f),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
 }
 
 @Preview
 @Composable
 fun CuadriculaPreview(){
-
+    Crea_una_cuadriculaTheme{
+        val cuadricula = Cuadricula(R.string.photography, 321, R.drawable.photography)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            CuadriculaCard(cuadricula = cuadricula)
+        }
+    }
 }
